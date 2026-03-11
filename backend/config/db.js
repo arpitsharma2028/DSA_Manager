997ee85eb0a16f5839286249eafeba383aaf587f
arpitsharma2028 <arpitsharma6105@gmail.com>
2026-03-11 22:37:13 +0530

    database shifted from mySQL to Postgres

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,10 @@
-import mysql from "mysql2/promise";
+import postgres from "postgres";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10
+const sql = postgres(process.env.DATABASE_URL, {
+  ssl: "require"
 });
 
-export default pool;
+export default sql;
