35648bd0ad389abded2ced6291359a526de26826
arpitsharma2028 <arpitsharma6105@gmail.com>
2026-03-12 02:07:42 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://your-frontend-domain.vercel.app"
-    ],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true
   })
 );
